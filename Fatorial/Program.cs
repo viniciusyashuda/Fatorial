1a5a3d206459e1fa39afd2be2efb953f75c70fd5
viniciusyashuda <viniciusyashuda008@gmail.com>
2022-03-08 09:34:27 -0300

    Add project files.

---
+++ Fatorial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number; long result;
+
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            result = fat(number);
+
+            Console.WriteLine($"{number}! = {result}");
+
+            static int fat(int number)
+            {
+
+                if(number == 0)
+                {
+
+                    return 1;
+
+                }
+                else
+                {
+
+                    return ((number) * (fat(number - 1)));
+
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
